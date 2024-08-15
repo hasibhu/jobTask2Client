0531fa87bb6c05cabb6a3e261fd32a6187811ec3
hasibhu <hasibul1987@gmail.com>
2024-08-15 09:54:17 +0200

    Several fila has been updated

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# Ecommerce website search and filter functionality
+# Job task 2
+
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -5,4 +6,5 @@ export default {
     extend: {},
   },
   plugins: [require("daisyui")],
+  
 };
